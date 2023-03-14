ea5ca7407c80cfd5e6eb326ad7a4859a14665e06
Francis Maina <sebastianfrancix@gmail.com>
2023-03-14 15:59:34 +0300

    added a #home route to StaticController and fixed a typo in  the cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,13 +3,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # Localhost
     allow do
-        origin "http://localhost:3000"
+        origins "http://localhost:3000"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 
     # app domain
     allow do
-        origin "https://gamers-signup254.netlify.com"
+        origins "https://gamers-signup254.netlify.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
