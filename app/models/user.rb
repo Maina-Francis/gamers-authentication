200d557ff26d948d7d146176fd030e3dbf9458f6
Francis Maina <sebastianfrancix@gmail.com>
2023-03-14 16:15:23 +0300

    encrypted the password_digest field and validated the presence of and uniqueness of email provided

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+    # encrypt the password_digest field using bcrypt
+    has_secure_password
+
+    # Validate presence and uniqueness of email
+    validates_presence_of :email
+    validates_uniqueness_of :email
 end
