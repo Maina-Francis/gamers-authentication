96b10a7ade3f5295d4ef263847043402832cb7be
Francis Maina <sebastianfrancix@gmail.com>
2023-03-15 05:54:05 +0300

    added logged_in and logout session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ActionController::Base
     skip_before_action :verify_authenticity_token
 
+    # import current_user_concern
+    include CurrentUserConcern
+
     def create
         # find user by email and authenticate their password
         user = User.find_by(email: params[:session][:email]).try(:authenticate, params[:session][:password])
@@ -21,5 +24,28 @@ class SessionsController < ActionController::Base
                 status: 401
             }
         end
+    end
+
+    # Logged_In
+    def logged_in
+        if @current_user
+        render json: {
+            logged_in: true,
+            user: @current_user
+        }
+        else
+        render json: {
+            logged_in: false
+        }
+        end
+    end
+
+    # log out
+    def logout 
+        reset_session
+        render json: {
+            status: 200,
+            logged_out: true
+        }
     end
 end
