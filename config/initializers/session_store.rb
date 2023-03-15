eed874091d1bc3e43525d6cd8759ffda182994db
Francis Maina <sebastianfrancix@gmail.com>
2023-03-15 05:58:53 +0300

    updated out cookies session_store to dynamically work whether on production mode or not

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
-# configure sessions to use cookies
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "gamers-signup254-api.render.com"
+if Rails.env == "production"
+    # configure sessions to use cookies
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "gamers-signup254-api.render.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
