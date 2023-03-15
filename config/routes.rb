c48129927a4ec706ccbda6aa675f85d448b45f97
Francis Maina <sebastianfrancix@gmail.com>
2023-03-15 05:43:23 +0300

    updated our routes to include logged_in and logged_out

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
 end
